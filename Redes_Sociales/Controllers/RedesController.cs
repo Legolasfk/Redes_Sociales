732975d06fc82bb738d7bbf9a4842c26e7d11977
Legolas <157653932+Legolasfk@users.noreply.github.com>
2025-10-24 21:46:07 -0600

    Feat: se crea el boton de kick

---
+++ Redes_Sociales/Controllers/RedesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebRedes.Controllers
+{
+    public class RedesController : Controller
+    {
+        public IActionResult Lienzo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Redes_Sociales/wwwroot/js/LienzoJS/Lienzo.js
@@ -0,0 +1,48 @@
+﻿/// <summary>
+/// JavaScript para el lienzo de redes sociales
+/// </summary>
+/// <createdate>10-10-2024</createdate>
+/// <author>René López Morales</author>
+/// <lastupdate>23-10-2025</lastupdate>
+
+const jsLienzo = {
+    botones: {
+        btnTwitch: '#btnTwitch',
+        btnKick2: '#btnKick', // Nuevo botón añadido
+        btnYouTube: '#btnYouTube',
+        btnTikTok: '#btnTikTok',
+        btnInstagram: '#btnInstagram',
+        btnDiscord: '#btnDiscord'
+    },
+    metodos: {
+        CargarPaginas: function (event) {
+            event.preventDefault();
+            const id = event.currentTarget.id;
+            console.log('Botón clickeado:', id); // Depuración
+            const urls = {
+                btnTwitch: 'https://www.twitch.tv/LEGOLASxd',
+                btnKick: 'https://kick.com/legolasfk', // URL para el nuevo botón
+                btnYouTube: 'https://www.youtube.com/@legolasfk',
+                btnTikTok: 'https://www.tiktok.com/@legolasfk',
+                btnInstagram: 'https://www.instagram.com/LEGOLASfk/',
+                btnDiscord: 'https://discord.gg/LEGOLAS'
+            };
+            if (urls[id]) {
+                console.log('Abriendo URL:', urls[id]); // Depuración
+                window.open(urls[id], '_blank');
+            } else {
+                console.warn(`No se encontró URL para el botón: ${id}`);
+            }
+        }
+    },
+    eventos: function () {
+        Object.values(jsLienzo.botones).forEach(selector => {
+            console.log('Asignando evento a:', selector); // Depuración
+            $(selector).on('click', jsLienzo.metodos.CargarPaginas);
+        });
+    }
+};
+
+$(function () {
+    jsLienzo.eventos();
+});
